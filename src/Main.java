3399e1c4655b6c914695c12c1695cd441eafdd50
DenisNPLS <sjemin1999@mail.ru>
2022-06-14 15:14:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 15680;
+        int rublesMiles = 20;
+
+
+        int bonus = ticketCost / rublesMiles;
+        System.out.println("Начисленно миллей за поездку: " + bonus);
+
+    }
+}
